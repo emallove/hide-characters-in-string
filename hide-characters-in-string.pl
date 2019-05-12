2ad85d8ac875e6cbea9a51247ed7c7acc42ab32d
Ethan Mallove <mallove@fas.harvard.edu>
2019-05-12 17:56:48 -0400

    Add hide-characters-in-string.pl

---
+++ hide-characters-in-string.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/env perl
+
+
+#
+# Script to hide random characters with underscores
+#
+
+
+# TODO: instead of going character-by-character, hide entire word tokens
+
+# perl /Users/hmdcadministrator/tmp/Script_to_hide_random_characters_in_underscores._.pl
+
+use strict;
+use Data::Dumper;
+use List::Util qw(shuffle);
+
+# Usage:
+if (! defined($ARGV[0])) {
+  print "Usage:
+$0 filename [how-much-is-hidden]
+
+how-much-is-hidden (the higher the value, the higher number of characters are hidden)
+  -  1: no characters hidden
+  -  2: half characters hidden
+  - 10: most characters hidden
+";
+}
+
+# Slurp the text file into a string
+my $filename = $ARGV[0];
+open my $fh, '<', $filename or die "Can't open file $!";
+my $file_content = do { local $/; <$fh> };
+
+# Fraction of characters to hide: 1 / ARGV[1]
+my $hide_fraction_denominator = $ARGV[1] || 2;
+
+my $len = length($file_content);
+my @random_number_seq = shuffle 0..$len;
+
+my $offset = ($len - int($len / $hide_fraction_denominator));
+print "\noffset = $offset\n\n";
+splice @random_number_seq, $offset;
+
+my %hash = map { $_ => 1 } @random_number_seq;
+
+# print Dumper @random_number_seq;
+
+my $i = 0;
+while ($file_content =~ /(.|\n|\r)/g) {
+    if (($hash{$i}) and ($& !~ /\s/)) {
+      print "_";
+    } else {
+      print "$&";
+    }
+    $i++;
+}
